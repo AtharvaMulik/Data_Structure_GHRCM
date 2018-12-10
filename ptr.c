7a7ba821d4d6d68238a640543412afc7e2895f5a
AtharvaMulik <45592988+AtharvaMulik@users.noreply.github.com>
2018-12-10 21:56:30 +0530

    Add files via upload

---
+++ ptr.c
@@ -0,0 +1,38 @@
+#sinclude<stdio.h>
+
+#include<stdlib.h>
+
+int main()
+
+{
+
+int n=17;
+
+int *p,q,**s;
+
+p=&n;
+
+q=*p;
+
+s=&p;
+
+
+printf("\np::%d",&(*p));
+
+printf("\nn::%d",n);
+
+printf("\np::%d",p);
+
+printf("\nq::%d",q);
+
+printf("\ns::%d",s);
+
+p++;
+
+printf("\np::%d",p);
+
+p=p+1;
+
+printf("\np+1::%d",p);
+
+}
