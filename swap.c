8ffdd2f9074452df9adf583d054da74ac949a36d
AtharvaMulik <45592988+AtharvaMulik@users.noreply.github.com>
2018-12-10 21:59:07 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void sswap(int x,int y)
+
+{
+
+int temp;
+
+temp=x;
+
+x=y;
+
+y=temp;
+
+printf("\nA = %d",x);
+
+printf("\nB = %d",y);
+
+}
+
+void main()
+
+{
+
+int a,b;
+
+printf("Enetr Value The A:-\n");
+
+scanf("%d",&a);
+
+printf("Enetr Value The B:-\n");
+
+scanf("%d",&b);
+sswap(a,b);
+
+}
