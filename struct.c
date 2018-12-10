8dde643dd22463540b827638079461d1a073d76a
AtharvaMulik <45592988+AtharvaMulik@users.noreply.github.com>
2018-12-10 21:51:27 +0530

    Add files via upload

---
+++ struct.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+
+struct student
+
+{
+
+int rollno;
+
+char name[20];
+
+}obj;
+
+void main()
+
+{
+
+
+int ch,n;
+
+do
+
+{
+
+printf("\n Enter your choice:-");
+
+scanf("%d",&ch);
+
+switch(ch)
+
+{
+
+case 1:
+
+printf("\n Enter Roll Number:-");
+
+scanf("%d",&obj.rollno);
+
+printf("\n Enter Name:-");
+
+scanf("%s",obj.name);
+
+break;
+
+case 2:
+
+printf("\n Roll No:-");
+
+printf("%d \t",obj.rollno);
+
+printf("\n Name:-");
+
+printf("%s \t",obj.name);
+
+}
+
+}
+while(ch!=3);
+
+}
+
