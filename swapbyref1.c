cde8f2114ddc0f40591806aefeefc370db402767
AtharvaMulik <45592988+AtharvaMulik@users.noreply.github.com>
2018-12-11 19:33:50 +0530

    Add files via upload

---
+++ swapbyref1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void swap1(int x,int y)
+{
+int temp;
+temp=x;
+x=y;
+y=temp;
+}
+
+void swap2(int *x,int *y)
+{
+int temp;
+temp=*x;
+*x=*y;
+*y=temp;
+}
+void main()
+{
+int a,b;
+printf("Enter the value of A:");
+scanf("%d",&a);
+printf("Enter the value of B:");
+scanf("%d",&b);
+swap1(a,b);
+printf("\nA = %d",a);
+printf("\nB = %d",b);
+swap2(&a,&b);
+printf("\nA = %d",x);
+printf("\nB = %d",y );
+}
