4a4ac46bb951ca8a9fadf2b578105f2f1c9efd11
AtharvaMulik <45592988+AtharvaMulik@users.noreply.github.com>
2018-12-17 16:33:51 +0530

    Add files via upload

---
+++ structpara.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct student
+{
+char name[20];
+int rollno;
+};
+struct student accept();
+int main()
+{
+struct student obj;
+obj=accept();
+printf("\n %s %d",obj.name,obj.rollno);
+}
+struct student accept()
+{
+struct student obj1;
+printf("\n Enter Name:");
+scanf("%s",obj.name);
+printf("\n Enter Roll_No:");
+scanf("%d",&obj.rollno);
+return(obj1);
+}
